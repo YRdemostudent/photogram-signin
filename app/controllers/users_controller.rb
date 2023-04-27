f22c46f818c25a8c72ce1fd6b928289b37444a28
Yanet Roman <yanet_0688@yahoo.com>
2023-04-27 20:19:51 +0000

    added the password authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,48 @@
 class UsersController < ApplicationController
 
+  def authenticate
+  # get the username from params 
+  un = params.fetch("input_username")
+
+  # get the password from params 
+  pw = params.fetch("input_password")
+
+  #look up the record from the db matching username 
+  user = User.where({:username => un }).at(0)
+
+  #if there's no record, redirect back to sign in form 
+  if user == nil
+    redirect_to("/user_sign_in", {:alert => "No one by that name 'round these parts" })
+  else
+  # if there is a record, check to see if password matches 
+   if user.authenticate(pw)
+  # if so, set the cookie 
+    session.store(:user_id, user.id)
+  
+  # redirect to homepage
+  redirect_to("/", {:notice => "Welcome back, " + user.username + "!" })
+   else
+  # if not, redirect back to sign in form 
+  redirect_to("/user_sign_in", {:alert => "Nice try, sucker!" })
+  end
+end 
+end
+
+  def burn_cookies
+    reset_session
+    
+    redirect_to("/", {:notice => "See ya later!"})
+  end 
+
   def registration_form
   
     render(:template => "users/signup_form.html.erb")
   end
 
+  def new_session_form
+  
+    render(:template => "users/signin_form.html.erb")
+  end
   def index
     @users = User.all.order({ :username => :asc })
 
@@ -28,6 +66,8 @@ class UsersController < ApplicationController
 #Making use of the if, else to display notice or alert when adding a new user 
     save_status = user.save
     if save_status == true
+      session.store(:user_id, user.id)
+
     redirect_to("/users/#{user.username}", {:notice => "Welcome, " + user.username + "!" })
     else 
       redirect_to("/user_sign_up", {:alert => user.errors.full_messages.to_sentence })
